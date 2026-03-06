7d270acb37fedd5702c759101bba97ff5fa7eb24
EdvardJNC <edvardks97@icloud.com>
2026-03-06 16:03:47 +0100

    feat(pipeline): integrate schema_extract node into LangGraph pipeline

--- pipeline/graph/graph.py
+++ pipeline/graph/graph.py
@@ -6,7 +6,7 @@ from langgraph.graph import StateGraph
 # Importamos el tipo de estado y los nodos
 from pipeline.graph.state import DocumentState
 from pipeline.graph.nodes import extract_node, clean_node, chunk_node, embedding_node
-
+from pipeline.graph.schema_extract_node import schema_extract_node
 
 def build_graph():
     """
@@ -21,6 +21,7 @@ def build_graph():
     workflow.add_node("clean", clean_node)
     workflow.add_node("chunk", chunk_node)
     workflow.add_node("embed", embedding_node)
+    workflow.add_node("schema_extract", schema_extract_node)
 
     # 3) Definimos cuál es el nodo inicial del flujo
     workflow.set_entry_point("extract")
@@ -29,6 +30,7 @@ def build_graph():
     workflow.add_edge("extract", "clean")
     workflow.add_edge("clean", "chunk")
     workflow.add_edge("chunk", "embed")
+    workflow.add_edge("embed", "schema_extract")
 
     # 5) Compilamos el grafo para poder hacer graph.invoke(state)
     graph = workflow.compile()
